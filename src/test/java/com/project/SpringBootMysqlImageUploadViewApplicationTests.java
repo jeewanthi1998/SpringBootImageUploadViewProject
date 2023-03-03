af8521b8bc5f70a94c61031ad499052fd469d541
Jeewanthi Gamage <62280919+jeewanthi1998@users.noreply.github.com>
2023-03-03 18:00:30 +0530

    Add files via upload

---
+++ src/test/java/com/project/SpringBootMysqlImageUploadViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlImageUploadViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
